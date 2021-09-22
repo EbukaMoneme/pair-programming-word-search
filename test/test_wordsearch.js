7abdcd8e9ed54c9205688028fa392ec19fe95dcc
EbukaMoneme <camoneme@gmail.com>
2021-09-22 21:36:46 +0000

    Completed wordsearch challenge

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -6,7 +6,7 @@ const wordSearch = require('../wordsearch.js')
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'W', 'C', 'F', 'R', 'A', 'K', 'K'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
@@ -35,4 +35,31 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'F', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'R', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'I', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'E', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'N', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'D', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRIEND')
+
+    assert.isTrue(result);
+  });
+  
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'FRIEND')
+    assert.isFalse(result);
+  });
+
+  it("should return Error if the array is undefined", function() {
+    const result = wordSearch(undefined, 'FRIEND')
+    assert.strictEqual('Error', result);
+  });
 });
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const horizontalJoin = (letters, word) => {
+  const horizontal = letters.map(ls => ls.join(''))
+  for (l of horizontal) {
+    if (l.includes(word)) return true;
+  }
+};
+
+const transpose = function (matrix) {
+  let newMatrix = [];
+  for (let row = 0; row < matrix[0].length; row++) {
+    newMatrix[row] = [];
+    for (let col = 0; col < matrix.length; col++) {
+      newMatrix[row].push(matrix[col][row]);
     }
-}
+  }
+  return newMatrix;
+};
+
+const wordSearch = (letters, word) => {
+  if (letters === undefined) {
+    return 'Error';
+  }
+  if (letters.length === 0) {
+    return false;
+  }
+  if (horizontalJoin(letters, word)) {
+    return true;
+  }
+  if (horizontalJoin(transpose(letters), word)) {
+    return true;
+  }
+  return false;
+};
 
 module.exports = wordSearch
